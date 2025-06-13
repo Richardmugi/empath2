d020544280b4f643a240f4330e2086bfef53de8c
Richardmugi <rmugisha927@gmail.com>
2025-06-13 11:26:38 +0300

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from starlette.middleware.sessions import SessionMiddleware
 app = FastAPI()
 
 # Mount all routes from main.py
-app.include_router(router)
+#app.include_router(router)
 
 # Enable session management (for login)
 app.add_middleware(SessionMiddleware, secret_key="supersecret")  
